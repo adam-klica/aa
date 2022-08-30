b8f26c12fb5ec566e4c12b5fff594ec82fcb92e4
Adam <adam.klica@ninjacloud.rs>
2022-08-30 19:13:12 +0200

    asd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import  { useState, useEffect } from 'react';
+import firebase from "firebase"
+function App() { 
+  const [podaci, setPodaci] = useState();
+  const [noviPodaci, setNoviPodaci] = useState()
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZWG86rlyAT0t6VmpFw9cZkft53DL6JjQ",
+    authDomain: "lemaa12.firebaseapp.com",
+    projectId: "lemaa12",
+    storageBucket: "lemaa12.appspot.com",
+    messagingSenderId: "132259430682",
+    appId: "1:132259430682:web:3911c6a5e6c26f0869aca3"
+  };
+  if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+  }else {
+    firebase.app(); // if already initialized, use that one
+ }
+  const data = firebase.database().ref("/");
 
-function App() {
+  useEffect(()=> {
+    data.on('value', (snap) => {
+      let data1  = snap.val();
+      let newState = [];
+        for(let i in data1) {
+          newState.push(data1[i]);
+        }
+        setPodaci(newState)
+    })
+    // console.log("podd", podaci)
+  }, [])
+  useEffect(() => {
+    const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
+      data.on('value', (snap) => {
+        let data1  = snap.val();
+        let newState = [];
+          for(let i in data1) {
+            newState.push(data1[i]);
+          }
+          setNoviPodaci(newState)
+          if(podaci.length !== noviPodaci.length){
+            setPodaci(noviPodaci)
+        }
+      })
+    }, 5000)
+  
+    return () => clearInterval(intervalId); //This is important
+   
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>{podaci}</p>
+
       </header>
     </div>
   );
